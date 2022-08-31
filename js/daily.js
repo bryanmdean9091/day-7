c5c526554e56c82f79d1db050ea19cb9e0db0705
bryanmdean9091 <bryanmdean9091@gmail.com>
2022-08-30 19:44:54 -0700

    first commit

---
+++ js/daily.js
@@ -0,0 +1,22 @@
+//Daily Challenge Day 7
+// Write a function called fizzbuzz that will accept no arguments
+// The goal of this function is to print out all numbers from 1 to 100 but with three exceptions:
+// For every number that is divisible by 3 and 5, console log "FizzBuzz"
+// For every number that is divisible by only 3 and not 5, console log "Fizz"
+// For every number that is divisible by only 5 and not 3, console log "Buzz"
+
+
+
+for (let i = 1; i <= 100; i++) { //created for loop that prints out No 1-100-i++ adds one number to thr previous
+    // console.log(i)
+    if (i % 3 === 0 && i % 5 === 0) {// first IF statement checking divisibilty by both 3 & 5
+        console.log('Fizzbuzz')// console logs Fizzbuzz if requirements met
+    }
+     if (i % 3 === 0 && i % 5 != 0) {// second IF statement-Only divisibility by 3 required
+        console.log('Fizz')//console logs Fizz if requirements met
+    }
+    else if (i % 3 != 0 && i % 5 === 0) {//ELSE IF statement-only divisibility by 5 required
+        console.log('Buzz')//console logs Buzz if requirements met
+    }
+    else console.log(i);//ELSE statement -console logs all numbers 1-100 not meeting above requirements
+}
---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+//  {
+// const h1 = document.createElement('h1');
+// h1.innerText = 'Goodbye Cruel World';
+// document.body.appendChild(h1);
+// addEventListener('click', () => {
+//     h1.style.color = 'blue';
+// });
+// }
+
+// const date = new Date();
+// console.log('get date', date.getFullYear());
+// console.log('get date', date.setMinutes());
+// console.log('get date', date.getFullYear());
+
+// //control flow
+
+
+// if (true) {
+//     console.log('run code');
+// }
+
+// if (false) {
+//     console.log('do not run code');
+// }
+// else if (true) {
+//     console.log('run code');
+// }
